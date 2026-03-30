1b9e67a71ca45a9ab40929c73f2e01fd95121f27
Ittesaf Yeasir Ithun <ittesafyeasir@iut-dhaka.edu>
2026-03-30 11:02:47 +0600

    replaced core modules

---
+++ code/Role.h
@@ -0,0 +1,12 @@
+#ifndef ROLE_H
+#define ROLE_H
+
+enum class Role {
+    Passenger      = 0,
+    Security       = 1,
+    SeniorSecurity = 2,
+    Admin          = 3,
+    Banned         = 4
+};
+
+#endif // ROLE_H
